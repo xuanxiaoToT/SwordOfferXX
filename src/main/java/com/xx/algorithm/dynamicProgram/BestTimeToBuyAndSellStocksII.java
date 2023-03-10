8a6f8dfacc1baccc338fedc1c3a187f802579be3
XuanXiaoToT <gaodftot@163.com>
2023-03-10 21:42:55 +0800

    玄霄-LeetCode 122. 买卖股票的最佳时机 II

--- src/main/java/com/xx/algorithm/dynamicProgram/BestTimeToBuyAndSellStocksII.java
+++ src/main/java/com/xx/algorithm/dynamicProgram/BestTimeToBuyAndSellStocksII.java
@@ -2,6 +2,8 @@ package com.xx.algorithm.dynamicProgram;
 
 import com.xx.Answer;
 
+import java.util.Arrays;
+
 /**
  * @author 玄霄
  * @CreateDate 2023/2/23
@@ -41,13 +43,28 @@ public class BestTimeToBuyAndSellStocksII implements Answer {
     /**
      * 解1：用动态规划来做。
      * 回溯法请参考backTracking里的解法。
-     * 每个点都有两个状态，
+     * <p>
+     * 考虑到「不能同时参与多笔交易」，因此每天交易结束后只可能存在手里有一支股票或者没有股票的状态。
+     * 定义状态 dp[i][0]表示第 i 天交易完后手里没有股票的最大利润，
+     * dp[i][1]表示第 i 天交易完后手里持有一支股票的最大利润（i 从 0开始）。
      */
     @Override
     public void answerOne() {
         int[] prices = initData();
         int[][] dp = new int[prices.length][2];
-
+        for (int i = 0; i < prices.length; i++) {
+            if (i < 1) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+            } else {
+                //Max(今天什么都不做最大利润与前一天相同，或者今天卖出即前一天持有一只股票的状态变为卖出)
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                //Max(今天什么都不做持有的股票与昨天一致，或者今天买入即前一天未持有一只股票的状态变为买入)
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            }
+        }
+        System.out.println(dp[prices.length-1][0]);
+        System.out.println(Arrays.deepToString(dp));
     }
 
 
@@ -57,7 +74,7 @@ public class BestTimeToBuyAndSellStocksII implements Answer {
     @Override
     public int[] initData() {
         //return new int[]{7, 1, 5, 3, 6, 4};
-        //return new int[]{1, 2, 3, 4, 5};
-        return new int[]{7, 6, 4, 3, 1};
+        return new int[]{1, 2, 3, 4, 5};
+        //return new int[]{7, 6, 4, 3, 1};
     }
 }
