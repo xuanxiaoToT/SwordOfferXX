9cf0b1a6f2d4bc55a966474a3d67a0c114cd14b2
XuanXiaoToT <gaodftot@163.com>
2023-03-14 22:18:47 +0800

    XuanXiao - LeetCode 48. 旋转图像

---
+++ src/main/java/com/xx/algorithm/other/RotateImage.java
@@ -0,0 +1,63 @@
+package com.xx.algorithm.other;
+
+import com.xx.Answer;
+
+/**
+ * @author XuanXiao
+ * @CreateDate 2023/3/14
+ * <p>
+ * 旋转图像
+ * LeetCode 48.
+ * <p>
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ * <p>
+ * 示例 1：
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[7,4,1],[8,5,2],[9,6,3]]
+ * <p>
+ * 示例 2：
+ * 输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ * 输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ */
+public class RotateImage implements Answer {
+
+    public static void main(String[] args) {
+        new RotateImage().answerOne();
+    }
+
+    /**
+     * 解1：
+     */
+    @Override
+    public void answerOne() {
+        int[][] matrix = initData();
+        //先转置矩阵,然后再反转每一行.
+        int n = matrix.length;
+
+        // transpose matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        // reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+    /**
+     * 输出数据
+     */
+    @Override
+    public int[][] initData() {
+        return null;
+    }
+}
