a93aa22242b4132ee4b06da40a2c42fecec05b6b
XuanXiaoToT <gaodftot@163.com>
2023-07-10 21:39:57 +0800

    XuanXiao - 新增笔记

--- src/main/java/com/xx/basicDs/heap/KDigitsWithTheHighestFrequency.java
+++ src/main/java/com/xx/basicDs/heap/KDigitsWithTheHighestFrequency.java
@@ -15,7 +15,6 @@ import java.util.PriorityQueue;
  * LeetCode 347. 前K个高频元素
  * <p>
  * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
- * 堆的最典型的用法
  * <p>
  * 示例 1:
  * 输入: nums = [1,1,1,2,2,3], k = 2
@@ -24,6 +23,8 @@ import java.util.PriorityQueue;
  * 示例 2:
  * 输入: nums = [1], k = 1
  * 输出: [1]
+ *
+ * 思路：堆的最典型的用法
  */
 public class KDigitsWithTheHighestFrequency implements Answer {
 
@@ -32,7 +33,7 @@ public class KDigitsWithTheHighestFrequency implements Answer {
     }
 
     /**
-     * something
+     * 用堆
      */
     @Override
     public void answerOne() {
