156dd4a843f37ddb719324432bb645b20a67b3e3
XuanXiaoToT <gaodftot@163.com>
2023-07-11 20:51:58 +0800

    XuanXiao - 新增题目，LeetCode 142. 环形链表 II

--- src/main/java/com/xx/basicDs/linked/EntryNodeInLinkedList.java
+++ src/main/java/com/xx/basicDs/linked/EntryNodeInLinkedList.java
@@ -4,11 +4,15 @@ import com.xx.Answer;
 import com.xx.domain.ListNode;
 import com.xx.util.DataFactory;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author XuanXiao
  * @CreateDate 2022/8/16
  * <p>
  * 链表中环的入口节点
+ * LeetCode 142. 环形链表 II
  * <p>
  * 如果一个链表中包含环，那么应该如何找出环的入口节点？
  * 从链表的头节点开始顺着next指针方向进入环的第1个节点为环
@@ -22,7 +26,6 @@ public class EntryNodeInLinkedList implements Answer {
 
     /**
      * 1.快慢法，快的每次走两格，慢的走一格，最后能追上。能追上只能证明存在环(还需要知晓环的大小。)
-     * 2.用map，比较花费空间。
      */
     @Override
     public void answerOne() {
@@ -41,6 +44,23 @@ public class EntryNodeInLinkedList implements Answer {
         // 求环的大小l
 
         // 双指针，快的先走l步，最后会跟慢的在入口处相遇。
+    }
+
+    /**
+     * 直接用set做。
+     */
+    public void answerTwo() {
+        Set<ListNode> visited = new HashSet<ListNode>();
+
+        ListNode node = initData();
+        while (node != null) {
+            if (visited.contains(node)) {
+                System.out.println(node);
+                return;
+            }
+            visited.add(node);
+            node = node.next;
+        }
     }
 
     /**
