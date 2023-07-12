4ad0a4ac8508f6262f69f4a77500da3c3ff62c4f
XuanXiaoToT <gaodftot@163.com>
2023-07-12 21:55:20 +0800

    XuanXiao - 新增题目，LeetCode 238. 除自身以外数组的乘积

---
+++ src/main/java/com/xx/algorithm/other/ProductExceptSelf.java
@@ -0,0 +1,76 @@
+package com.xx.algorithm.other;
+
+import com.xx.Answer;
+
+import java.util.Arrays;
+
+/**
+ * @author XuanXiao
+ * @CreateDate 2023/7/12
+ * <p>
+ * 除自身以外数组的乘积
+ * LeetCode 238
+ * <p>
+ * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外
+ * 其余各元素的乘积。题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的
+ * 乘积都在 32 位 整数范围内。请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * <p>
+ * 进阶：你可以在 O(1) 的额外空间复杂度内完成这个题目吗？
+ * （ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+ * <p>
+ * 示例 1:
+ * 输入: nums = [1,2,3,4]
+ * 输出: [24,12,8,6]
+ * <p>
+ * 示例 2:
+ * 输入: nums = [-1,1,0,-3,3]
+ * 输出: [0,0,9,0,0]
+ */
+public class ProductExceptSelf implements Answer {
+
+    public static void main(String[] args) {
+        new ProductExceptSelf().answerOne();
+    }
+
+    /**
+     * 解1：
+     */
+    @Override
+    public void answerOne() {
+        int[] inputData = initData();
+        int[] left = new int[inputData.length];
+        int[] right = new int[inputData.length];
+        int[] result = new int[inputData.length];
+        int leftTemp = 1;
+        for (int i = 0; i < inputData.length; i++) {
+            left[i] = leftTemp * inputData[i];
+            leftTemp = leftTemp * inputData[i];
+        }
+        int rightTemp = 1;
+        for (int i = inputData.length - 1; i >= 0; i--) {
+            right[i] = rightTemp * inputData[i];
+            rightTemp = rightTemp * inputData[i];
+        }
+
+        for (int i = 0; i < inputData.length; i++) {
+            if (i == 0) {
+                result[i] = right[i + 1];
+            } else {
+                if (i == inputData.length - 1) {
+                    result[i] = left[i - 1];
+                } else {
+                    result[i] = left[i - 1] * right[i + 1];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(result));
+    }
+
+    /**
+     * 输出数据
+     */
+    @Override
+    public int[] initData() {
+        return new int[]{1, 2, 3, 4};
+    }
+}
