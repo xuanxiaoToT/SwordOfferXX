619a16e8a53821080fd57eb334175f5e6f061573
XuanXiaoToT <gaodftot@163.com>
2023-08-08 19:35:56 +0800

    XuanXiao - 新增题目，LeetCode 189  轮转数组

--- src/main/java/com/xx/basicDs/array/ArrangeArrayIntoSmallestNumber.java
+++ src/main/java/com/xx/basicDs/array/ArrangeArrayIntoSmallestNumber.java
@@ -6,6 +6,9 @@ import com.xx.Answer;
  * @author XuanXiao
  * @CreateDate 2022/7/6
  * <p>
+ * 把数组排成最小的数
+ * 剑指 Offer 45
+ * <p>
  * 输入一个非负整数数组nums，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
  * 0 < nums.length <= 100
  * <p>
