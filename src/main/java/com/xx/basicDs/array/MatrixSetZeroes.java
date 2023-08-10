b8db7852459f93a7cbf14b34b4bef46078767b6a
XuanXiaoToT <gaodftot@163.com>
2023-08-10 21:23:29 +0800

    XuanXiao - 新增题目，LeetCode 73 矩阵置零

--- src/main/java/com/xx/basicDs/array/MatrixSetZeroes.java
+++ src/main/java/com/xx/basicDs/array/MatrixSetZeroes.java
@@ -15,6 +15,17 @@ import java.util.HashSet;
  * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。
  * 请使用 原地 算法。
  * <p>
+ * 进阶：
+ * 一个直观的解决方案是使用 O(mn) 的额外空间，但这并不是一个好的解决方案。
+ * 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+ * 你能想出一个仅使用常量空间的解决方案吗？
+ * <p>
+ * 提示：
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -231 <= matrix[i][j] <= 231 - 1
+ * <p>
  * 示例 1：
  * 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
  * 输出：[[1,0,1],[0,0,0],[1,0,1]]
@@ -55,6 +66,16 @@ public class MatrixSetZeroes implements Answer {
             }
         });
         System.out.println(Arrays.deepToString(matrix));
+    }
+
+    /**
+     * O(m + n) ：即用一个行，一个列来标识需要置0的行和列。
+     * 常量：用二进制位，来标识，因为M和N最大也就是200.
+     * 一个200位的byte值即可。
+     * 做法略
+     */
+    public void answerTwo() {
+
     }
 
     /**
