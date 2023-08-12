76ee5809f9c79ebf9c8c6c82768adef8abf4ab21
XuanXiaoToT <gaodftot@163.com>
2023-08-12 11:55:13 +0800

    XuanXiao - 新增题目，LeetCode 240. 搜索二维矩阵 II

--- src/main/java/com/xx/algorithm/search/Find2DArray.java
+++ src/main/java/com/xx/algorithm/search/Find2DArray.java
@@ -1,25 +1,38 @@
 package com.xx.algorithm.search;
 
+import com.xx.Answer;
+
 /**
  * @author XuanXiao
  * @CreateDate 2022/6/14
+ * <p>
  * 二维数组中的查找
+ * leetCode 240. 搜索二维矩阵 II
+ * <p>
  * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  */
-public class Find2DArray {
+public class Find2DArray implements Answer {
 
     public static void main(String[] args) {
         Find2DArray find2DArray = new Find2DArray();
         //目标值
-        int targetData = 9;
+        int targetData = -10;
         find2DArray.answerTwo(targetData);
+    }
+
+    /**
+     * 解1：先对行二分，再对列二分的方法是错误的~！
+     */
+    @Override
+    public void answerOne() {
+        // 错误解法略
     }
 
     // 如果 左下角元素 大于了目标值，则目标值一定在该行的上方， 左下角元素 所在行可以消去。
     // 如果 左下角元素 小于了目标值，则目标值一定在该列的右方， 左下角元素 所在列可以消去。
     private void answerTwo(int targetData) {
-        int[][] dataArray = initTestDate();
+        int[][] dataArray = initData();
         int rowId = dataArray.length - 1;
         int colId = 0;
         while (colId < dataArray[0].length && rowId >= 0) {
@@ -30,29 +43,27 @@ public class Find2DArray {
             if (dataArray[rowId][colId] > targetData) {
                 rowId--;
             }
-            if (dataArray[rowId][colId] < targetData) {
+            if (rowId >= 0 && dataArray[rowId][colId] < targetData) {
                 colId++;
             }
         }
         System.out.println("AAA");
     }
-    
-    private int[][] initTestDate() {
-        //    [
-        //   [1,   4,  7, 11, 15],
-        //   [2,   5,  8, 12, 19],
-        //   [3,   6,  9, 16, 22],
-        //   [10, 13, 14, 17, 24],
-        //   [18, 21, 23, 26, 30]
-        // ]
-        // 审题！
-        int[][] testDate = {{1, 4, 7, 11, 15},
-                {2, 5, 8, 12, 19},
-                {3, 6, 9, 16, 22},
-                {10, 13, 14, 17, 24},
-                {18, 21, 23, 26, 30}};
 
-        System.out.println(testDate.toString());
-        return testDate;
+
+    /**
+     * 输出数据
+     */
+    @Override
+    public int[][] initData() {
+        //int[][] testDate = {{1, 4, 7, 11, 15},
+        //        {2, 5, 8, 12, 19},
+        //        {3, 6, 9, 16, 22},
+        //        {10, 13, 14, 17, 24},
+        //        {18, 21, 23, 26, 30}};
+
+        //return testDate;
+
+        return new int[][]{{-5}};
     }
 }
