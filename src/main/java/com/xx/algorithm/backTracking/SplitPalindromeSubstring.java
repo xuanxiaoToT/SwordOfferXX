1c0652e0ebc4d838f3c733762f8ea03567da9e6b
XuanXiaoToT <gaodftot@163.com>
2023-09-06 21:26:49 +0800

    XuanXiao - 新增题目，LeetCode 131. 分割回文串

--- src/main/java/com/xx/algorithm/backTracking/SplitPalindromeSubstring.java
+++ src/main/java/com/xx/algorithm/backTracking/SplitPalindromeSubstring.java
@@ -10,6 +10,7 @@ import java.util.List;
  * @CreateDate 2022/11/24
  * <p>
  * 分割回文子字符串
+ * LeetCode 131. 分割回文串
  * <p>
  * 输入一个字符串，要求将它分割成若干子字符串，使每
  * 个子字符串都是回文。请列出所有可能的分割方法。
@@ -17,6 +18,14 @@ import java.util.List;
  * 例如，输入"google"，将输出3种符合条件的分割方法，分别是
  * ["g"，"o"，"o"，"g"，"l"，"e"]、["g"，"oo"，"g"，"l"，"e"]
  * 和["goog"，"l"，"e"]。
+ * <p>
+ * 示例 1：
+ * 输入：s = "aab"
+ * 输出：[["a","a","b"],["aa","b"]]
+ * <p>
+ * 示例 2：
+ * 输入：s = "a"
+ * 输出：[["a"]]
  */
 public class SplitPalindromeSubstring implements Answer {
 
@@ -25,7 +34,7 @@ public class SplitPalindromeSubstring implements Answer {
     }
 
     /**
-     * 1.用普通方法做，其本质就是找出所有的回文子串。
+     * 1.用普通回溯法做，其本质就是找出所有的回文子串。
      */
     @Override
     public void answerOne() {
