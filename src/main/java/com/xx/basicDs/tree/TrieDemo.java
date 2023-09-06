d6844d37c7b07a5e06188aafe6aaeaec5de02619
XuanXiaoToT <gaodftot@163.com>
2023-09-06 21:22:48 +0800

    XuanXiao - 新增题目，LeetCode 208. 前缀树

--- src/main/java/com/xx/basicDs/tree/TrieDemo.java
+++ src/main/java/com/xx/basicDs/tree/TrieDemo.java
@@ -6,12 +6,30 @@ import com.xx.domain.TrieNode;
  * @author XuanXiao
  * @CreateDate 2022/10/12
  * <p>
+ * 实现 Trie (前缀树)
+ * LeetCode 208.
+ * <p>
  * 请设计实现一棵前缀树Trie，它有如下操作。
  * ● 函数insert，在前缀树中添加一个字符串。
  * ● 函数search，查找字符串。如果前缀树中包含该字符串，则返回
  * true；否则返回false。
  * ● 函数startWith，查找字符串前缀。如果前缀树中包含以该前缀开
  * 头的字符串，则返回true；否则返回false。
+ * <p>
+ * 示例：
+ * 输入
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * 输出
+ * [null, null, true, false, true, null, true]
+ * 解释
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 True
+ * trie.search("app");     // 返回 False
+ * trie.startsWith("app"); // 返回 True
+ * trie.insert("app");
+ * trie.search("app");     // 返回 True
  */
 public class TrieDemo {
 
