c98452d82da4db80670c36e66f787cd3271acd23
XuanXiaoToT <gaodftot@163.com>
2023-09-07 21:41:46 +0800

    XuanXiao - 新增题目，LeetCode 35. 搜索插入位置

--- src/main/java/com/xx/algorithm/search/FindInsertPosition.java
+++ src/main/java/com/xx/algorithm/search/FindInsertPosition.java
@@ -5,7 +5,10 @@ import com.xx.Answer;
 /**
  * @author XuanXiao
  * @CreateDate 2022/10/18
+ * <p>
  * 查找插入位置
+ * LeetCode 35. 搜索插入位置
+ * <p>
  * 输入一个排序的整数数组nums和一个目标值t，如果数组
  * nums中包含t，则返回t在数组中的下标；如果数组nums中不包含t，
  * 则返回将t按顺序插入数组nums中的下标。假设数组中没有相同的数
@@ -13,6 +16,18 @@ import com.xx.Answer;
  * <p>
  * 例如，输入数组nums为[1，3，6，8]，如果目标值t为3，则输
  * 出1；如果t为5，则返回2。
+ * <p>
+ * 示例 1:
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ * <p>
+ * 示例 2:
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ * <p>
+ * 示例 3:
+ * 输入: nums = [1,3,5,6], target = 7
+ * 输出: 4
  */
 public class FindInsertPosition implements Answer {
 
