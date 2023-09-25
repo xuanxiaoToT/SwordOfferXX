77e9fe7cd943f4d4fcb44e4185b8ad8d612debcb
XuanXiaoToT <gaodftot@163.com>
2023-09-25 21:09:36 +0800

    XuanXiao - 新增题目，LeetCode 55. 跳跃游戏

--- src/main/java/com/xx/algorithm/dynamicProgram/JumpGame.java
+++ src/main/java/com/xx/algorithm/dynamicProgram/JumpGame.java
@@ -50,17 +50,30 @@ public class JumpGame implements Answer {
         System.out.println(Arrays.toString(dp));
     }
 
-    /**
-     * 动态规划
-     */
-    public void answerTwo() {
-
-    }
-
     private void fillJumpPoints(int[] dp, int start, int maxDis) {
         for (int i = start + 1; i <= start + maxDis && i < dp.length; i++) {
             dp[i] = 1;
         }
+    }
+
+    /**
+     * 贪心法
+     * 我们依次遍历数组中的每一个位置，并实时维护 最远可以到达的位置。对于当前遍历到的位置 x，如果它在 最远可以到达的位置 的范围内，
+     * 那么我们就可以从起点通过若干次跳跃到达该位置，因此我们可以用 x+nums[x] 更新 最远可以到达的位置。
+     */
+    public boolean answerTwo(int[] nums) {
+        int n = nums.length;
+        int rightMax = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightMax) {
+                rightMax = Math.max(rightMax, i + nums[i]);
+                if (rightMax >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
     }
 
     /**
