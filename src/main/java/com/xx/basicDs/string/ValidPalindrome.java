2a8eb774e74651ed972d1701852098cfe9e143de
XuanXiaoToT <gaodftot@163.com>
2023-10-07 20:30:16 +0800

    XuanXiao - 新增题目，LeetCode 验证回文串

--- src/main/java/com/xx/basicDs/string/ValidPalindrome.java
+++ src/main/java/com/xx/basicDs/string/ValidPalindrome.java
@@ -5,7 +5,9 @@ import com.xx.Answer;
 /**
  * @author XuanXiao
  * @CreateDate 2022/8/10
- * 有效的回文
+ * <p>
+ * 验证回文串
+ * LeetCode 125.  简单
  * <p>
  * 给定一个字符串，请判断它是不是回文。假设只需要考
  * 虑字母和数字字符，并忽略大小写。例如，"Was it a cat I
@@ -17,6 +19,22 @@ import com.xx.Answer;
  * true。
  * 找到不一致处的点，然后跳过left点或跳过right点，判断剩下的点是否是回文即可。
  * 也是用双指针
+ * <p>
+ * 示例 1：
+ * 输入: s = "A man, a plan, a canal: Panama"
+ * 输出：true
+ * 解释："amanaplanacanalpanama" 是回文串。
+ * <p>
+ * 示例 2：
+ * 输入：s = "race a car"
+ * 输出：false
+ * 解释："raceacar" 不是回文串。
+ * <p>
+ * 示例 3：
+ * 输入：s = " "
+ * 输出：true
+ * 解释：在移除非字母数字字符之后，s 是一个空字符串 "" 。
+ * 由于空字符串正着反着读都一样，所以是回文串。
  */
 public class ValidPalindrome implements Answer {
 
@@ -30,14 +48,22 @@ public class ValidPalindrome implements Answer {
     @Override
     public void answerOne() {
         String oriStr = initData();
+        if (oriStr.length() == 0) {
+            System.out.println(true);
+            return;
+        }
         int left = 0;
         int right = oriStr.length() - 1;
         while (left < right) {
-            while (!Character.isLetterOrDigit(oriStr.charAt(left))) {
+            while (left < oriStr.length() && !Character.isLetterOrDigit(oriStr.charAt(left))) {
                 left++;
             }
-            while (!Character.isLetterOrDigit(oriStr.charAt(right))) {
+            while (right >= 0 && !Character.isLetterOrDigit(oriStr.charAt(right))) {
                 right--;
+            }
+            if (left >= oriStr.length() || right < 0) {
+                System.out.println(false);
+                return;
             }
             if (Character.toLowerCase(oriStr.charAt(left)) == Character.toLowerCase(oriStr.charAt(right))) {
                 left++;
@@ -55,6 +81,9 @@ public class ValidPalindrome implements Answer {
      */
     @Override
     public String initData() {
-        return "Was it a cat I saw？";
+        //return "Was it a cat I saw？";
+        // return "A man，a plan,a canal: panama";
+        //return "race a car";
+        return "++";
     }
 }
