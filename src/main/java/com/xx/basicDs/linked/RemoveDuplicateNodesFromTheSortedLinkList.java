03a9ffc52bf5c800ca133abb8bed7d5a4818322d
XuanXiaoToT <gaodftot@163.com>
2023-10-19 21:23:11 +0800

    XuanXiao - 新增题目，LeetCode 删除排序链表中的重复元素II

--- src/main/java/com/xx/basicDs/linked/RemoveDuplicateNodesFromTheSortedLinkList.java
+++ src/main/java/com/xx/basicDs/linked/RemoveDuplicateNodesFromTheSortedLinkList.java
@@ -86,6 +86,37 @@ public class RemoveDuplicateNodesFromTheSortedLinkList implements Answer {
         System.out.println(head);
     }
 
+    /**
+     * 官解：我们从指针 cur 指向链表的哑节点，随后开始对链表进行遍历。如果当前 cur.next 与 cur.next.next 对应的元素相同，
+     * 那么我们就需要将 cur.next 以及所有后面拥有相同元素值的链表节点全部删除。我们记下这个元素值 x，随后不断将 cur.next 从链表中移除，
+     * 直到 cur.next 为空节点或者其元素值不等于 x 为止。此时，我们将链表中所有元素值为 x 的节点全部删除。
+     * 如果当前 cur.next对应的元素不相同，那么说明链表中只有一个元素值为 cur.next 的节点，那么我们就可以将 cur 指向 cur.next。
+     * <p>
+     * 当遍历完整个链表之后，我们返回链表的的哑节点的下一个节点 dummy.next 即可。
+     * 链接：https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.value == cur.next.next.value) {
+                int x = cur.next.value;
+                while (cur.next != null && cur.next.value == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+
     @Override
     public ListNode initData() {
         ListNode root = new ListNode(1);
