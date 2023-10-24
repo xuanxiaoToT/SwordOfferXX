e2ef834336dc188cae68500c3dca7060b6eb3630
XuanXiaoToT <gaodftot@163.com>
2023-10-24 22:22:00 +0800

    XuanXiao - 新增题目，LeetCode 二叉树的锯齿形层序遍历

--- src/main/java/com/xx/basicDs/tree/ZigzagTransverseOutTree.java
+++ src/main/java/com/xx/basicDs/tree/ZigzagTransverseOutTree.java
@@ -10,13 +10,34 @@ import java.util.Queue;
 /**
  * @author XuanXiao
  * @CreateDate 2022/6/27
+ * <p>
+ * 二叉树的锯齿形层序遍历
+ * LeetCode  103  Medium
+ * <p>
  * 从上到下，从左到右  "之"字型打印二叉树
+ * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * <p>
+ * 示例 1：
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[20,9],[15,7]]
+ * <p>
+ * 示例 2：
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * <p>
+ * 示例 3：
+ * 输入：root = []
+ * 输出：[]
+ * <p>
+ * 提示：
+ * 树中节点数目在范围 [0, 2000] 内
+ * -100 <= Node.val <= 100
  */
-public class TransverseOutTree2 implements Answer {
+public class ZigzagTransverseOutTree implements Answer {
 
     public static void main(String[] args) {
-        TransverseOutTree2 transverseOutTree2 = new TransverseOutTree2();
-        transverseOutTree2.answerOne();
+        ZigzagTransverseOutTree zigzagTransverseOutTree = new ZigzagTransverseOutTree();
+        zigzagTransverseOutTree.answerOne();
     }
 
     @Override
