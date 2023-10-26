f080a4499a968740515decfbd1844416e80a08a1
XuanXiaoToT <gaodftot@163.com>
2023-10-26 21:54:09 +0800

    XuanXiao - 新增题目，LeetCode 克隆图

--- src/main/java/com/xx/basicDs/graph/CloningOfGraphs.java
+++ src/main/java/com/xx/basicDs/graph/CloningOfGraphs.java
@@ -133,23 +133,22 @@ public class CloningOfGraphs implements Answer {
             return node;
         }
 
-        HashMap<Node, Node> visited = new HashMap();
+        HashMap<Node, Node> visited = new HashMap<>();
 
         // 将题目给定的节点添加到队列
         LinkedList<Node> queue = new LinkedList<Node>();
         queue.add(node);
         // 克隆第一个节点并存储到哈希表中
-        visited.put(node, new Node(node.val, new ArrayList()));
+        visited.put(node, new Node(node.val, new ArrayList<>()));
 
         // 广度优先搜索
         while (!queue.isEmpty()) {
             // 取出队列的头节点
-            Node n = queue.remove();
-            // 遍历该节点的邻居
+            Node n = queue.poll();
             for (Node neighbor : n.neighbors) {
                 if (!visited.containsKey(neighbor)) {
                     // 如果没有被访问过，就克隆并存储在哈希表中
-                    visited.put(neighbor, new Node(neighbor.val, new ArrayList()));
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
                     // 将邻居节点加入队列中
                     queue.add(neighbor);
                 }
