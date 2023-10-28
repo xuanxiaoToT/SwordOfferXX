f8984191d445e6a8ee31655afec64737fc4234ad
XuanXiaoToT <gaodftot@163.com>
2023-10-28 11:55:50 +0800

    XuanXiao - 新增题目，LeetCode 旋转图像

--- src/main/java/com/xx/algorithm/other/RotateImage.java
+++ src/main/java/com/xx/algorithm/other/RotateImage.java
@@ -7,7 +7,7 @@ import com.xx.Answer;
  * @CreateDate 2023/3/14
  * <p>
  * 旋转图像
- * LeetCode 48.
+ * LeetCode 48. Medium
  * <p>
  * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
  * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
@@ -27,12 +27,12 @@ public class RotateImage implements Answer {
     }
 
     /**
-     * 解1：
+     * 解1：先转置矩阵,然后再反转每一行.
      */
     @Override
     public void answerOne() {
         int[][] matrix = initData();
-        //先转置矩阵,然后再反转每一行.
+
         int n = matrix.length;
 
         // transpose matrix
