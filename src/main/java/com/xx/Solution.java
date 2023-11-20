288a605a4a3b33109507d5572bd288c0f25651f7
XuanXiaoToT <gaodftot@163.com>
2023-11-20 22:14:01 +0800

    XuanXiao - 新增题目，LeetCode 使三个字符串相等

--- src/main/java/com/xx/Solution.java
+++ src/main/java/com/xx/Solution.java
@@ -12,6 +12,35 @@ public class Solution {
 
     public void answer() {
 
+
+        //System.out.println(maximumXorProduct(1, 6, 3));
     }
+
+    public int maximumXorProduct(long a, long b, int n) {
+        int yu = 1000000000;
+        String aBin = Long.toBinaryString(a);
+        String bBin = Long.toBinaryString(b);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int aIndex = aBin.length() - n + i;
+            char ai = aIndex >= 0 ? aBin.charAt(aIndex) : '0';
+            int bIndex = bBin.length() - n + i;
+            char bi = bIndex >= 0 ? bBin.charAt(bIndex) : '0';
+            if (ai == '0' && bi == '0') {
+                sb.append('1');
+            }
+            if (ai == '1' && bi == '1') {
+                sb.append('0');
+            }
+            if (ai == '0' && bi == '1' || ai == '1' && bi == '0') {
+                sb.append('1');
+            }
+        }
+        long x = Long.parseLong(sb.toString(), 2);
+        return (int) (((a ^ x) * (b ^ x)) % yu);
+    }
+
+
+
 
 }
