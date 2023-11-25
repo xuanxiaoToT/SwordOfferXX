dbe0b9ec7795772bb12205f71c1da60374619ac8
XuanXiaoToT <gaodftot@163.com>
2023-11-25 13:26:16 +0800

    XuanXiao - 新增题目，LeetCode 数组中的第K个最大元素

--- src/main/java/com/xx/basicDs/heap/TheKLargestNumberOfDataStreams.java
+++ src/main/java/com/xx/basicDs/heap/TheKLargestNumberOfDataStreams.java
@@ -2,21 +2,35 @@ package com.xx.basicDs.heap;
 
 import com.xx.Answer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 /**
  * @author XuanXiao
  * @CreateDate 2022/10/8
  * <p>
- * 数据流的第k大数字
- * LeetCode 215
+ * 数组中的第K个最大元素
+ * LeetCode 215 Medium
  * <p>
- * 请设计一个类型KthLargest，它每次从一个数据流中读
- * 取一个数字，并得出数据流已经读取的数字中第k（k≥1）大的数
- * 字。该类型的构造函数有两个参数：一个是整数k，另一个是包含数
- * 据流中最开始数字的整数数组nums（假设数组nums的长度大于k）。
- * 该类型还有一个函数add，用来添加数据流中的新数字并返回数据流
- * 中已经读取的数字的第k大数字。
+ * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+ * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * <p>
+ * 示例 1:
+ * 输入: [3,2,1,5,6,4], k = 2
+ * 输出: 5
+ * <p>
+ * 示例 2:
+ * 输入: [3,2,3,1,2,4,5,5,6], k = 4
+ * 输出: 4
+ * <p>
+ * 提示：
+ * 1 <= k <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * <p>
+ * Tag: 最小堆 分治法 快速排序 计数排序
  */
 public class TheKLargestNumberOfDataStreams implements Answer {
 
@@ -28,18 +42,81 @@ public class TheKLargestNumberOfDataStreams implements Answer {
     /**
      * 求第K大，用最小堆。
      * 求第K小，用最大堆。
+     * 时间复杂度O(N*LogN)
      */
     @Override
     public void answerOne() {
         int[] nums = initData();
         int k = 5;
-        PriorityQueue<Integer> heap = new PriorityQueue<>(k);
+        System.out.println(findKthLargest(nums, k));
+    }
 
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(k);
         for (int num : nums) {
             add(heap, num, k);
         }
+        return heap.peek() == null ? 0 : heap.peek();
+    }
 
-        System.out.println(heap.peek());
+    /**
+     * 方法2：计数排序
+     * 注意题目：
+     * -10^4 <= nums[i] <= 10^4
+     */
+    public int findKthLargest2(int[] nums, int k) {
+        int n = nums.length;
+        int[] f = new int[21000];
+        for (int i = 0; i < n; i++) {
+            //+10000是将负数往右移
+            f[nums[i] + 10000]++;
+        }
+        int cnt = 0;
+        for (int i = 21000 - 1; i >= 0; i--) {
+            cnt += f[i];
+            if (cnt >= k) {
+                return i - 10000;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 方法3：分治，快排
+     * 参看快排
+     */
+    public int findKthLargest3(int[] nums, int k) {
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums) {
+            numList.add(num);
+        }
+        return quickSelect(numList, k);
+    }
+
+    private int quickSelect(List<Integer> nums, int k) {
+        // 随机选择基准数
+        Random rand = new Random();
+        int pivot = nums.get(rand.nextInt(nums.size()));
+        // 将大于、小于、等于 pivot 的元素划分至 big, small, equal 中
+        List<Integer> big = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> small = new ArrayList<>();
+        for (int num : nums) {
+            if (num > pivot)
+                big.add(num);
+            else if (num < pivot)
+                small.add(num);
+            else
+                equal.add(num);
+        }
+        // 第 k 大元素在 big 中，递归划分
+        if (k <= big.size())
+            return quickSelect(big, k);
+        // 第 k 大元素在 small 中，递归划分
+        if (nums.size() - small.size() < k)
+            return quickSelect(small, k - nums.size() + small.size());
+        // 第 k 大元素在 equal 中，直接返回 pivot
+        return pivot;
     }
 
     private void add(PriorityQueue<Integer> heap, int num, int k) {
