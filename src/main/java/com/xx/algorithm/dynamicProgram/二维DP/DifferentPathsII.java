d730c78e71e99d4d04b1a392caba8c9b9023c8bb
XuanXiaoToT <gaodftot@163.com>
2023-12-11 22:04:48 +0800

    XuanXiao - 新增题目，LeetCode 不同路径II

---
+++ src/main/java/com/xx/algorithm/dynamicProgram/二维DP/DifferentPathsII.java
@@ -0,0 +1,89 @@
+package com.xx.algorithm.dynamicProgram.二维DP;
+
+import com.xx.Answer;
+
+/**
+ * @author XuanXiao
+ * @CreateDate 2023/12/11
+ * <p>
+ * 不同路径II
+ * LeetCode 63. Medium
+ * <p>
+ * 提示
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * <p>
+ * 示例 1：
+ * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * 输出：2
+ * 解释：3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ * <p>
+ * 示例 2：
+ * 输入：obstacleGrid = [[0,1],[0,0]]
+ * 输出：1
+ * <p>
+ * 提示：
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 <= m, n <= 100
+ * obstacleGrid[i][j] 为 0 或 1
+ * <p>
+ * Tag: 二维动态规划
+ */
+public class DifferentPathsII implements Answer {
+    public static void main(String[] args) {
+        new DifferentPathsII().answerOne();
+    }
+
+    /**
+     * 解1：
+     */
+    @Override
+    public void answerOne() {
+        int[][] obstacleGrid = new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+    }
+
+    /**
+     * 简单动态分布
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        dp[0][0] = 1;
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            for (int j = 0; j < obstacleGrid[0].length; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j > 0) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+                if (j == 0 && i > 0) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                if (i > 0 && j > 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+    }
+
+
+    /**
+     * 输出数据
+     */
+    @Override
+    public Object initData() {
+        return null;
+    }
+}
