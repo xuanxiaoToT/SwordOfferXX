5e34d6e935f59e1808789e186541b3fb6ea2204b
XuanXiaoToT <gaodftot@163.com>
2023-12-11 21:15:17 +0800

    XuanXiao - 新增题目，LeetCode 零钱兑换

--- src/main/java/com/xx/algorithm/dynamicProgram/MinimumNumberOfCoins.java
+++ src/main/java/com/xx/algorithm/dynamicProgram/MinimumNumberOfCoins.java
@@ -9,12 +9,29 @@ import java.util.Arrays;
  * @CreateDate 2022/12/13
  * <p>
  * 最少的硬币个数
- * LeetCode 322
+ * LeetCode 322  零钱兑换  Medium
  * <p>
  * 给定不同面额的硬币 coins 和金额 amount，计算凑成总金额所需的最少的硬币个数
  * <p>
- * 输入: coins = [1, 2, 5], amount = 11 输出: 3 解释: 11 = 5 + 5 + 1
- * coins = [1, 3, 9, 10], amount = 15 输出: 3
+ * 示例 1：
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * <p>
+ * 示例 2：
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * <p>
+ * 示例 3：
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ * <p>
+ * 提示：
+ * 1 <= coins.length <= 12
+ * 1 <= coins[i] <= 2^31 - 1
+ * 0 <= amount <= 10^4
+ * <p>
+ * Tag: 动态规划  记忆化搜索
  */
 public class MinimumNumberOfCoins implements Answer {
 
@@ -23,40 +40,32 @@ public class MinimumNumberOfCoins implements Answer {
     }
 
     /**
-     * * 如果将每种面额的硬币看成一种物品，而将目标总额看成
-     * * 背包的容量，那么这个问题等价于求将背包放满时物品的最少件数。
-     * * 值得注意的是，这里每种面额的硬币可以使用任意多次，因此这个问
-     * * 题不再是0-1背包问题，而是一个无界背包问题（也叫完全背包问
-     * * 题）。
+     * 如果将每种面额的硬币看成一种物品，而将目标总额看成
+     * 背包的容量，那么这个问题等价于求将背包放满时物品的最少件数。
+     * 值得注意的是，这里每种面额的硬币可以使用任意多次，因此这个问
+     * 题不再是0-1背包问题，而是一个无界背包问题（也叫完全背包问题）。
      */
     @Override
     public void answerOne() {
-        int[] coins = initData();
-        Arrays.sort(coins);
-        int amount = 15;
-        int[] dp = new int[amount + 1];
-        for (int i = 0; i < dp.length; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int coin : coins) {
-                if (i >= coin) {
-                    if (dp[i - coin] < min) {
-                        min = dp[i - coin];
-                    }
-                }
-            }
-            if (min == Integer.MAX_VALUE) {
-                dp[i] = 0;
-            } else {
-                dp[i] = min + 1;
-            }
-        }
-
-        System.out.println(Arrays.toString(dp));
+        int[] coins = new int[]{2};
+        int amount = 3;
+        System.out.println(coinChange(coins, amount));
     }
 
-    private void answerTwo() {
-        // 采用贪心算法，直接先拿大的，不够再拿后面的。以此类推。
-        //  算法略
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.parallelSetAll(dp, i -> max);
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; i++) {
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[amount] == max ? -1 : dp[amount];
     }
 
     /**
