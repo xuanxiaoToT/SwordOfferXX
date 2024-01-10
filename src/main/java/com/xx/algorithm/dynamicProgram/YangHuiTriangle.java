6ecdb9c32184ea990fa31d5d71a203e0cd2e3b17
XuanXiaoToT <gaodftot@163.com>
2024-01-10 21:00:14 +0800

    XuanXiao - 新增题目，LeetCode 三个数的最大乘积

--- src/main/java/com/xx/algorithm/dynamicProgram/YangHuiTriangle.java
+++ src/main/java/com/xx/algorithm/dynamicProgram/YangHuiTriangle.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @CreateDate 2023/9/11
  * <p>
  * 杨辉三角
- * LeetCode 118.
+ * LeetCode 118. Easy
  * <p>
  * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
  * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
@@ -22,6 +22,8 @@ import java.util.List;
  * 示例 2:
  * 输入: numRows = 1
  * 输出: [[1]]
+ * <p>
+ * Tag：二维数组
  */
 public class YangHuiTriangle implements Answer {
 
@@ -39,19 +41,19 @@ public class YangHuiTriangle implements Answer {
     }
 
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
         for (int i = 0; i < numRows; ++i) {
             List<Integer> row = new ArrayList<Integer>();
             for (int j = 0; j <= i; ++j) {
                 if (j == 0 || j == i) {
                     row.add(1);
                 } else {
-                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
                 }
             }
-            ret.add(row);
+            result.add(row);
         }
-        return ret;
+        return result;
     }
 
 
