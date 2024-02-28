ff6a1c9762ea639c759e4c8aed4b3f07011b4745
XuanXiaoToT <gaodftot@163.com>
2024-02-28 21:44:34 +0800

    XuanXiao - 新增题目，LeetCode 使用最小花费爬楼梯

--- src/main/java/com/xx/algorithm/dynamicProgram/简单DP/MinimumCostOfClimbingStairs.java
+++ src/main/java/com/xx/algorithm/dynamicProgram/简单DP/MinimumCostOfClimbingStairs.java
@@ -8,16 +8,37 @@ import java.util.Arrays;
  * @author XuanXiao
  * @CreateDate 2022/11/30
  * <p>
- * 爬楼梯的最少成本
+ * 使用最小花费爬楼梯
+ * LeetCode 746. Medium
  * <p>
- * 一个数组cost的所有数字都是正数，它的第i个数字表示
- * 在一个楼梯的第i级台阶往上爬的成本，在支付了成本cost[i]之后
- * 可以从第i级台阶往上爬1级或2级。假设台阶至少有2级，既可以从
- * 第0级台阶出发，也可以从第1级台阶出发，请计算爬上该楼梯的最
- * 少成本。
- * 例如，输入数组[1，100，1，1，100，1]，则爬上该楼梯
- * 的最少成本是4，分别经过下标为0、2、3、5的这4级台阶，如图
- * 14.1所示。
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ * <p>
+ * 示例 1：
+ * 输入：cost = [10,15,20]
+ * 输出：15
+ * 解释：你将从下标为 1 的台阶开始。
+ * - 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+ * 总花费为 15 。
+ * <p>
+ * 示例 2：
+ * 输入：cost = [1,100,1,1,1,100,1,1,100,1]
+ * 输出：6
+ * 解释：你将从下标为 0 的台阶开始。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 2 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 4 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 6 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达下标为 7 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 9 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达楼梯顶部。
+ * 总花费为 6 。
+ * <p>
+ * 提示：
+ * 2 <= cost.length <= 1000
+ * 0 <= cost[i] <= 999
+ * <p>
+ * Tag: 动态规划一维
  */
 public class MinimumCostOfClimbingStairs implements Answer {
 
@@ -31,17 +52,19 @@ public class MinimumCostOfClimbingStairs implements Answer {
     @Override
     public void answerOne() {
         int[] cost = initData();
-        // 其实只需要用到i-1 和 i-2的值，可以优化空间复杂度
-        int[] dp = new int[cost.length];
-        dp[0] = cost[0];
-        for (int i = 1; i < cost.length; i++) {
-            if (i > 1) {
-                dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
-            } else {
-                dp[i] = dp[i - 1] + cost[i];
-            }
+        System.out.println(minCostClimbingStairs(cost));
+    }
+
+    // 其实只需要用到i-1 和 i-2的值，可以优化空间复杂度
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i < cost.length + 1; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
         System.out.println(Arrays.toString(dp));
+        return dp[cost.length];
     }
 
     /**
@@ -49,6 +72,6 @@ public class MinimumCostOfClimbingStairs implements Answer {
      */
     @Override
     public int[] initData() {
-        return new int[]{1, 100, 1, 1, 100, 1};
+        return new int[]{10, 15, 20};
     }
 }
