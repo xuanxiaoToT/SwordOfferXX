103ec52a9eff845372005ff4097c94a9c6a31c70
XuanXiaoToT <gaodftot@163.com>
2026-01-10 21:58:36 +0800

    LeetCode  按摩师

--- src/main/java/com/xx/basicDs/array/SumOfThreeNumbers.java
+++ src/main/java/com/xx/basicDs/array/SumOfThreeNumbers.java
@@ -9,7 +9,7 @@ import java.util.*;
  * @CreateDate 2023/3/17
  * <p>
  * 三数之和
- * LeetCode 015
+ * LeetCode 015 Medium
  * <p>
  * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i],
  * nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
@@ -71,7 +71,7 @@ public class SumOfThreeNumbers implements Answer {
                 }
             }
         }
-        //fixme：解决重复问题
+        // fixme：解决重复问题
         System.out.println(result);
     }
 
@@ -91,9 +91,9 @@ public class SumOfThreeNumbers implements Answer {
     public void answerTwo() {
         int[] nums = initData();
         List<List<Integer>> result = new ArrayList<>();
-        //排序
+        // 排序
         Arrays.sort(nums);
-        //双指针
+        // 双指针
         int len = nums.length;
         for (int i = 0; i < len; ++i) {
             if (nums[i] > 0) {
@@ -127,6 +127,46 @@ public class SumOfThreeNumbers implements Answer {
             }
         }
         System.out.println(result);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
     }
 
     /**
