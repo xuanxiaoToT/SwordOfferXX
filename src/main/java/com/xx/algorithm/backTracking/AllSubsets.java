ad2c6a6675837f61eed0388151091ead506c25b4
XuanXiaoToT <gaodftot@163.com>
2026-01-16 22:05:37 +0800

    LeetCode 所有子集

--- src/main/java/com/xx/algorithm/backTracking/AllSubsets.java
+++ src/main/java/com/xx/algorithm/backTracking/AllSubsets.java
@@ -3,24 +3,26 @@ package com.xx.algorithm.backTracking;
 import com.xx.Answer;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author XuanXiao
  * @CreateDate 2022/10/26
  * <p>
  * 所有子集
- * LeetCode 78. 子集 Medium
+ * 子集
+ * 幂集
+ * LeetCode 78. Medium
  * <p>
  * 输入一个不含重复数字的数据集合，请找出它的所有子集。
  * <p>
  * 例如，数据集合[1，2]有4个子集，分别是[]、[1]、[2]和[1，2]。
+ * <p>
+ * Tag：回溯
  */
 public class AllSubsets implements Answer {
 
-    private Set<List<Integer>> result = new HashSet<>();
+    private List<List<Integer>> result = new ArrayList<>();
 
     public static void main(String[] args) {
         new AllSubsets().answerOne();
@@ -32,8 +34,13 @@ public class AllSubsets implements Answer {
     @Override
     public void answerOne() {
         int[] nums = initData();
-        myDiGui(new ArrayList<>(), 0, nums);
+        subsets(nums);
         System.out.println(result);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        myDiGui(new ArrayList<>(), 0, nums);
+        return result;
     }
 
     public void myDiGui(List<Integer> temp, int i, int[] data) {
@@ -41,14 +48,13 @@ public class AllSubsets implements Answer {
             result.add(new ArrayList<>(temp));
             return;
         }
-        //选
+        // 选
         temp.add(data[i]);
         myDiGui(temp, i + 1, data);
         temp.remove(temp.size() - 1);
 
-        //不选
+        // 不选
         myDiGui(temp, i + 1, data);
-
     }
 
     /**
