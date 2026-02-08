b107a8468db76385b42e6e57b88dc55a7ff66ef9
XuanXiaoToT <gaodftot@163.com>
2026-02-08 16:38:19 +0800

    LeetCode

---
+++ src/main/java/com/xx/temp/week/WeekExamFour.java
@@ -0,0 +1,19 @@
+package com.xx.temp.week;
+
+import com.xx.Answer;
+
+public class WeekExamFour implements Answer {
+    public static void main(String[] args) {
+        new WeekExamFour().answer();
+    }
+
+    @Override
+    public void answer() {
+
+    }
+
+    @Override
+    public Object initData() {
+        return null;
+    }
+}
